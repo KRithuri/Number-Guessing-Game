1ef7c8437a1ae7c79f9c74cfab488e61600f95cb
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-23 20:16:30 +0200

    final output to user

--- guess.py/guess.py
+++ guess.py/guess.py
@@ -14,23 +14,25 @@ play_option = input(f'Hi, {player_name}, would you like to play' f' the guessing
 if play_option.lower() != 'y':
     print('That is cool, Thanks!')
     exit()
-    
-while True:
-    #increment guesses variable
-    guesses += 1
-    #ask user for input
-    user_guesses = int(input("Guess a number between 1 and 10: "))
 
-    #code to run if user gueess = computer guess
-    if user_guesses == random_number:
-        print("You got it!")
+else:
+    while True:
+        #increment guesses variable
+        guesses += 1
+        #ask user for input
+        user_guesses = int(input("Guess a number between 1 and 10: "))
+
+        #code to run if user gueess = computer guess
+        if user_guesses == random_number:
+            print("You got it!")
+            
+        #code to run if user guess > computer guess
+        elif user_guesses >random_number:
+            print("It is lower...") 
+            
+        #code to run if user guess < computer guess
+        else:
+            print("It is highher...")
         
-    #code to run if user guess > computer guess
-    elif user_guesses >random_number:
-        print("It is lower...") 
-        
-    #code to run if user guess < computer guess
-    else:
-        print("It is highher...")
         
 print("You got it in", guesses, "guesses")
