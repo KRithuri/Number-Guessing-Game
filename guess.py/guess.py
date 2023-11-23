e3cb7c4352cfc682feb700fd2259278c76439504
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-23 19:56:05 +0200

    ask user for input

--- guess.py/guess.py
+++ guess.py/guess.py
@@ -5,3 +5,8 @@ import random
 random_number = random.randint(1,10)
 
 guesses = 0
+
+while True:
+    guesses += 1
+    #ask user for input
+    user_guesses = int(input("Guess a number between 1 and 10: "))
