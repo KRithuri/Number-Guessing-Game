7bd9324d296d70c2fc79c3b88fbfd2abd2974b57
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-23 20:23:36 +0200

    main funvtion to make code neater

--- guess.py/guess.py
+++ guess.py/guess.py
@@ -1,38 +1,40 @@
-#import random module 
-import random
+def play_game():
+    #import random module 
+    import random
 
-#generate random number for the computer
-random_number = random.randint(1,10)
+    #generate random number for the computer
+    random_number = random.randint(1,10)
 
-guesses = 0
+    guesses = 0
 
-#ask user for name
-print('Hello user! Welcome to the guessing game!')
-player_name = input('What is your name? ')
+    #ask user for name
+    print('Hello user! Welcome to the guessing game!')
+    player_name = input('What is your name? ')
 
-play_option = input(f'Hi, {player_name}, would you like to play' f' the guessing game?(Enter Y/N): ')
-if play_option.lower() != 'y':
-    print('That is cool, Thanks!')
-    exit()
+    play_option = input(f'Hi, {player_name}, would you like to play' f' the guessing game?(Enter Y/N): ')
+    if play_option.lower() != 'y':
+        print('That is cool, Thanks!')
+        exit()
 
-else:
-    while True:
-        #increment guesses variable
-        guesses += 1
-        #ask user for input
-        user_guesses = int(input("Guess a number between 1 and 10: "))
+    else:
+        while True:
+            #increment guesses variable
+            guesses += 1
+            #ask user for input
+            user_guesses = int(input("Guess a number between 1 and 10: "))
 
-        #code to run if user gueess = computer guess
-        if user_guesses == random_number:
-            print("You got it!")
+            #code to run if user gueess = computer guess
+            if user_guesses == random_number:
+                print("You got it in", guesses, "guesses")
+                exit()
+                
+            #code to run if user guess > computer guess
+            elif user_guesses >random_number:
+                print("It is lower...") 
+                
+            #code to run if user guess < computer guess
+            else:
+                print("It is highher...")
             
-        #code to run if user guess > computer guess
-        elif user_guesses >random_number:
-            print("It is lower...") 
             
-        #code to run if user guess < computer guess
-        else:
-            print("It is highher...")
-        
-        
-print("You got it in", guesses, "guesses")
+play_game()
