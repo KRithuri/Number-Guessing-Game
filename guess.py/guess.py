ec956f7ec1c1b7574a9cdd27d9e1ee7e6b8c9d3a
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-23 21:10:32 +0200

    code to run if user does not want to play again

--- guess.py/guess.py
+++ guess.py/guess.py
@@ -11,24 +11,23 @@ def show_score():
         print(f'The current high score is' f' {min(attempt_list)} attempts')
         
 def play_game():
-   
-    import random
-
     #generate random number for the computer
     random_number = random.randint(1,10)
-
     guesses = 0
 
     #ask user for name
     print('Hello user! Welcome to the guessing game!')
     player_name = input('What is your name? ')
 
+    #ask user if they want to pay the game
     play_option = input(f'Hi, {player_name}, would you like to play' f' the guessing game?(Enter Y/N): ')
     if play_option.lower() != 'y':
         print('That is cool, Thanks!')
         exit()
 
     else:
+        show_score()
+        
         while True:
             #increment guesses variable
             guesses += 1
@@ -38,7 +37,20 @@ def play_game():
             #code to run if user gueess = computer guess
             if user_guesses == random_number:
                 print("You got it in", guesses, "guesses")
-                exit()
+                attempt_list.append(guesses)
+                
+                play_option = input('Would you like to play again? (Enter Y/N)')
+
+                #code to run if user does not want to play again
+                if play_option.lower() != 'y':
+                    print('That is cool, Thanks!')
+                    break
+                else:
+                    guesses = 0
+                    random_number = random.randint(1, 10)
+                    show_score()
+                    continue
+            
                 
             #code to run if user guess > computer guess
             elif user_guesses >random_number:
