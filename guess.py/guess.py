06b0c81bbeeabd23fb03c612389ae2621b8bfe11
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-23 20:00:10 +0200

    increment guesses variable

--- guess.py/guess.py
+++ guess.py/guess.py
@@ -7,6 +7,10 @@ random_number = random.randint(1,10)
 guesses = 0
 
 while True:
+    #increment guesses variable
     guesses += 1
     #ask user for input
     user_guesses = int(input("Guess a number between 1 and 10: "))
+
+    #if user_guesses == random_number:
+       # print("You got it!")
