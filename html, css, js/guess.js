44eba7081d7ef827e6b3f0990f4eefbcabb38eac
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-25 01:48:33 +0200

    code to run if user guess > random number

--- html, css, js/guess.js
+++ html, css, js/guess.js
@@ -24,6 +24,7 @@ function play(){
             message1.textContent = "No of guesses: " + number_of_guesses;
             message2.textContent = "Guessed number are: " + guessed_numbers;
         }
+        //code to run if user guess > random number
         else if(user_guess > randomNumber){
             result.textContent = "It is lower..."
             message1.textContent = "No of guesses: " + number_of_guesses;
