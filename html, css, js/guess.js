ad46f3da08c1849dd3b33b1946139532ede0ddb5
Karabo Rithuri <rithurikarabo@gmail.com>
2023-11-25 00:22:55 +0200

    generate random number

--- html, css, js/guess.js
+++ html, css, js/guess.js
@@ -1,2 +1,5 @@
 const guessInput = document.getElementById('guess-input'); //user guess input
-const guessBtn = document.getElementById('guess-btn'); 
+const guessBtn = document.getElementById('guess-btn'); //guess button
+const result = document.getElementById('result');
+
+const randomNumber = Math.floorloor(Math.random() * 100) + 1;
